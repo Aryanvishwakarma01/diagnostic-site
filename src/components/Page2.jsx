d9ba0e8443b0b79b7793bc1fbfda0b91e0064d22
Aryanvishwakarma01 <aryanvish86@gmail.com>
2025-08-01 08:51:32 +0530

    changes

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Ultrasound from './Ultrasound.png';
 const Page2 = () => {
   return (
-    <div className="page2 flex w-full p-15 justify-between items-center h-[80vh]">
-    <div className="page2_left w-[50%] flex flex-col gap-6">
+    <div className="page2 relative flex w-full p-15 justify-between items-center h-[80vh]">
+    <div className="page2_left relative w-[50%] flex flex-col gap-6">
       <h1 className='text-4xl font-bold'>Welcome to <span className='text-orange-500'>Harsh Lab & Diagnostic</span></h1>
       <p className='text-xl text-gray-900'>Harsh Lab & Diagnostic, located in [Sector/Area Name, City], is a trusted name among diagnostic centers in the region. Known for its accurate and timely services, it has quickly earned a strong reputation. The center is supported by a dedicated team committed to providing quality care to patients from both nearby and distant areas.</p>
       <div className="page2_buttons flex items-center gap-3">
