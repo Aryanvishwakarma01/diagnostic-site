31c5928353de789dbf5a8fa9bd5cadc4bbe41c38
Aryanvishwakarma01 <aryanvish86@gmail.com>
2025-08-01 12:32:58 +0530

    major changes

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -20,11 +20,11 @@ const About = () => {
     </h1>
     <p className='text-gray-800 py-10 sm:py-5'>
       Harsh Lab & Diagnostic is a trusted name in medical diagnostics, committed to delivering accurate, timely, and affordable diagnostic solutions. Since our inception, we have focused on combining advanced technology with compassionate care to support patients and healthcare providers in making informed decisions.
-      <br /><br />
+      <br />
       At Harsh Diagnostic, we offer a comprehensive range of diagnostic services including pathology, radiology, imaging, and specialized tests – all under one roof. Our facility is equipped with state-of-the-art machines and backed by a team of experienced pathologists, radiologists, lab technicians, and support staff who are dedicated to upholding the highest standards of quality and precision.
-      <br /><br />
+      <br />
       We understand the critical role diagnostics play in healthcare, and that’s why we adhere strictly to industry protocols and maintain complete transparency in every report we deliver. Our goal is to make diagnostics more accessible and patient-friendly, without compromising on reliability.
-      <br /><br />
+      <br />
       Whether it's routine health checkups, advanced imaging, or specialized investigations, Harsh Diagnostic stands as your reliable partner in health. Your well-being is our priority, and we strive to serve with integrity, care, and excellence.
     </p>
   </div>
