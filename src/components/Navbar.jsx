525320455f6c1bc2e973eb3ffb1a506e42c8b3a8
Aryanvishwakarma01 <aryanvish86@gmail.com>
2025-08-01 11:55:43 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from './logo.png';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import RightNav from './RightNav';
 import { IoIosMenu } from "react-icons/io";
 import { IoCloseSharp } from "react-icons/io5";
@@ -21,11 +21,11 @@ function Navbar() {
             <div  className={`fixed w-full top-0 h-screen -left-[100%] z-50  bg-black/50 transition-all duration-300 ${phoneOpen && "left-0"}`}>
                 <ul className="phone_menu z-51 list-none bg-white w-[60%] h-screen flex flex-col items-start px-5 py-15 gap-5 text-base font-medium text-black cursor-pointer relative">
                 <IoCloseSharp onClick={() => setPhoneOpen(false)} className='absolute text-2xl border right-5 top-5' />
-                    <li className="text-orange-500 border-b-2 hover:text-orange-600 transition"><Link to="/">HOME</Link></li>
-                    <li className="hover:text-orange-500 border-b-2 transition"><Link to="../About">ABOUT US</Link></li>
                     <li className="hover:text-orange-500 border-b-2 transition"><Link to="../Facility">FACILITIES</Link></li>
+                    <li className="text-orange-500 border-b-2 hover:text-orange-600 transition"><Link to="/">HOME</Link></li>
                     {/* <li className="hover:text-orange-500 transition">DIAGNOSTIC SERVICES</li> */}
                     <li className="hover:text-orange-500 border-b-2 transition"><Link to="../ContactUs">CONTACT US</Link></li>
+                    <li className="hover:text-orange-500 border-b-2 transition"><Link to="../About">ABOUT US</Link></li>
                     
                 </ul>
             </div>
--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 function RightNav() {
   return (
     <div className='right_nav'>
-        <ul className=" list-none flex items-center gap-10 text-base font-medium text-black cursor-pointer">
-                <li className="text-orange-500 hover:text-orange-600 transition"><Link to="/">HOME</Link></li>
-                <li className="hover:text-orange-500 transition"><Link to="../About">ABOUT US</Link></li>
-                <li className="hover:text-orange-500 transition"><Link to="../Facility">FACILITIES</Link></li>
-                {/* <li className="hover:text-orange-500 transition">DIAGNOSTIC SERVICES</li> */}
-                <li className="hover:text-orange-500 transition"><Link to="../ContactUs">CONTACT US</Link></li>
-                <li className="hover:bg-orange-500 transition bg-green-600 p-2 text-white transition-all duration-400"><a href="tel:9646548046"> +91-9646548046</a></li>
-            </ul>
+      <ul className=" list-none flex items-center gap-10 text-base font-medium text-black cursor-pointer">
+        <li className="transition">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-orange-500" : "text-black hover:text-orange-500"
+            }
+          >
+            HOME
+          </NavLink>
+        </li>
+
+
+        <li className="transition">
+          <NavLink
+            to="/Facility"
+            className={({ isActive }) =>
+              isActive ? "text-orange-500" : "text-black hover:text-orange-500"
+            }
+          >
+            FACILITIES
+          </NavLink>
+        </li>
+
+        <li className="transition">
+          <NavLink
+            to="/ContactUs"
+            className={({ isActive }) =>
+              isActive ? "text-orange-500" : "text-black hover:text-orange-500"
+            }
+          >
+            CONTACT US
+          </NavLink>
+        </li>
+        <li className="transition">
+          <NavLink
+            to="/About"
+            className={({ isActive }) =>
+              isActive ? "text-orange-500" : "text-black hover:text-orange-500"
+            }
+          >
+            ABOUT US
+          </NavLink>
+        </li>
+      </ul>
     </div>
   )
 }
