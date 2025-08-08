237c244be7cd5e59c3872d345883e9a0f63b2a95
Aryanvishwakarma01 <aryanvish86@gmail.com>
2025-08-08 21:06:10 +0530

    changes

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -12,19 +12,19 @@ const Facilities = () => {
   }
   return (
     <div className="facilities w-full bg-gray-200 px-[30px] py-[25px] sm:p-15 flex flex-col gap-8 sm:gap-15">
-      <h1 className="text-4xl text-center font-bold">OUR <span className="text-orange-500">FACILITIES</span></h1>
-      <div id="facilities" className="flex items-center justify-center flex-col gap-15 sm:flex-row sm:gap-10">
+      <h1 className="text-[30px] sm:text-4xl text-center font-bold">OUR <span className="text-orange-500">FACILITIES</span></h1>
+      <div id="facilities" className="flex items-center justify-center flex-col gap-15 sm:flex-row sm:gap-10 cursor-pointer">
         {facilitiesArr.map((item, index) => (
-          <div key={index} className="facility_card h-[400px] sm:h-[250px] w-[300px] sm:w-[350px] bg-white rounded-xl overflow-hidden shadow-md mx-auto">
-            <img src={item.image} className="h-[92%] sm:h-[85%] w-full object-cover" alt={item.name} />
-            <h1 className="text-center py-2 text-[16px] sm:text-xl font-bold">{item.name}</h1>
+          <div key={index} className="facility_card h-[250px] sm:h-[250px] w-[200px] sm:w-[350px] bg-white rounded-xl overflow-hidden shadow-md mx-auto">
+            <img src={item.image} className="h-[88%] sm:h-[85%] w-full object-cover" alt={item.name} />
+            <h1 className="text-center py-2 text-[14px] sm:text-xl font-bold">{item.name}</h1>
           </div>
         ))}
       </div>
 
       <div className='flex items-center justify-center gap-5'>
-        <button onClick={reloadPage} className='border px-8 py-3 rounded-4xl bg-green-700 hover:bg-red-500 transition duration-300 text-white text-[18px] font-bold cursor-pointer'><NavLink to="/contact-us">Book Appointment</NavLink></button>
-        <button className='border px-8 py-3 rounded-4xl bg-green-700 hover:bg-orange-500 transition duration-300  text-white text-[18px] font-bold cursor-pointer'><a href="tel:9646548046"><i className="ri-phone-fill"></i> Call Now</a>
+        <button onClick={reloadPage} className='border px-6 py-3 rounded-4xl bg-red-500 hover:bg-red-700 border-none transition duration-300 text-white text-[14px] sm:text-[18px] font-bold cursor-pointer'><NavLink to="/contact-us">Book Appointment</NavLink></button>
+        <button className='border px-6 py-3 rounded-4xl bg-green-700 hover:bg-orange-500 transition duration-300  text-white text-[14px] sm:text-[18px]  font-bold cursor-pointer'><a href="tel:9646548046"><i className="ri-phone-fill"></i> Call Now</a>
         </button>
       </div>
     </div>
--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -4,7 +4,7 @@ const Page2 = () => {
   return (
     <div className="page2 relative flex w-full flex-col sm:flex-row px-2 sm:px-15 py-10 sm:py-15 justify-between items-center">
     <div className="page2_left relative w-[100%] sm:w-[50%] p-[10px] sm:p-0 flex flex-col gap-6">
-      <h1 className='text-4xl sm:text-[35px] font-bold text-center w-full  sm:text-center'>Welcome to <span className='text-orange-500'>Harsha Lab & Diagnostic</span></h1>
+      <h1 className='text-2xl sm:text-[35px] font-bold text-center w-full  sm:text-center'>Welcome to <span className='text-orange-500'>Harsha Lab & Diagnostic</span></h1>
       <p className='text-[15px] text-center sm:text-justify sm:text-xl text-gray-900 '>Harsh Lab & Diagnostic, located in [Sector/Area Name, City], is a trusted name among diagnostic centers in the region. Known for its accurate and timely services, it has quickly earned a strong reputation. The center is supported by a dedicated team committed to providing quality care to patients from both nearby and distant areas.</p>
       <div className="page2_buttons flex items-center justify-center sm:justify-start gap-3">
         <button className='border py-4 px-10 rounded-4xl text-white bg-green-700 hover:bg-orange-500 transition duration-300 font-bold cursor-pointer'>Read More</button>
